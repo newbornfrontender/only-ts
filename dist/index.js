cd313431f02ee41349c33bd73bf02d70d1256ed6
Infant Frontender <newbornfrontender@gmail.com>
2019-08-09 17:08:18 +0300

    feat: init

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const sum_1 = __importDefault(require("./sum/sum"));
+console.log(sum_1.default(2, 2));
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import sum from './sum/sum';
+
+console.log(sum(2, 2));
